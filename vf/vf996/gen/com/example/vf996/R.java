5dc7f89ca6a099123ebd641b6518f9a1a3064b5b
wangamanda <wangamanda.lu@gmail.com>
2014-12-11 15:19:37 -0500

    second

--- vf/vf996/gen/com/example/vf996/R.java
+++ vf/vf996/gen/com/example/vf996/R.java
@@ -1035,8 +1035,8 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_search=0x7f05003d;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_search=0x7f05003e;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1058,6 +1058,7 @@ containing a value of this type.
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
+        public static final int my_text=0x7f05003d;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
@@ -1138,8 +1139,9 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int fragment_payment=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int fragment_category=0x7f030018;
+        public static final int fragment_payment=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1187,6 +1189,7 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_network=0x7f0a0010;
         public static final int action_settings=0x7f0a000f;
+        public static final int add_wallet=0x7f0a0014;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
         public static final int payment=0x7f0a0013;
